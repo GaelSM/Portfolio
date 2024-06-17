8125b58f6babe051d7a50ab893aaf77a004a27e3
Segura Mendoza Gael Alejandro <gaelsegura.mendoza@gmail.com>
2024-06-17 13:55:11 -0600

    Adding header tag to improve semantic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import "./Navbar.css"
 
 export default function Header() {
   return (
-    <nav className="navbar">
-      <ul>
-        <li><a href="#about-me"> Sobre mí </a></li>
-        <li><a href="#experience"> Experiencia </a></li>
-        <li><a href="#projects"> Proyectos </a></li>
-        <li><a href="#presentation"> Contacto </a></li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="navbar">
+        <ul>
+          <li><a href="#about-me"> Sobre mí </a></li>
+          <li><a href="#experience"> Experiencia </a></li>
+          <li><a href="#projects"> Proyectos </a></li>
+          <li><a href="#presentation"> Contacto </a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
