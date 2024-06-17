33426facdfdee604185ff112c73604e495f9fa89
Segura Mendoza Gael Alejandro <gaelsegura.mendoza@gmail.com>
2024-06-17 14:16:05 -0600

    Improve semantic

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -2,16 +2,16 @@ import Social from "./Social";
 
 export default function Presentation() {
   return (
-    <div className="presentation" id="presentation">
+    <section className="presentation" id="presentation">
       <div className="text">
         <h1> Gael Alejandro Segura Mendoza</h1>
         <h3> Desarrollador Web </h3>
       </div>
-      <div className="socials">
+      <address className="socials">
         <Social src="https://www.linkedin.com/in/gaelsm/" icon="fa-brands fa-linkedin" text="LinkedIn" />
         <Social src="https://github.com/GaelSM" icon="fa-brands fa-github" text="Github" />
         <Social src="mailto:gaelsegura.mendoza@gmail.com" icon="fa-solid fa-envelope" text="Correo" />
-      </div>
-    </div>
+      </address>
+    </section>
   )
 }
