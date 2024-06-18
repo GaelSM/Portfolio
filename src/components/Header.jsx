98192703c5227c2173d4872b9373f1e2934cd468
Segura Mendoza Gael Alejandro <gaelsegura.mendoza@gmail.com>
2024-06-17 19:46:50 -0600

    Change about section to profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <nav className="navbar">
         <ul>
-          <li><a href="#about-me"> Sobre mí </a></li>
+          <li><a href="#about-me"> Perfil </a></li>
           <li><a href="#experience"> Experiencia </a></li>
           <li><a href="#projects"> Proyectos </a></li>
           <li><a href="#presentation"> Contacto </a></li>
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section id="about-me">
+      <h2> Perfil </h2>
+      <div>
+        <p>
+          Desarrollador Web Front-End con conocimientos sólidos en <strong>HTML, CSS, JavaScript</strong> y <strong> React </strong>.
+        </p>
+      </div>
+    </section>
+  )
+}
