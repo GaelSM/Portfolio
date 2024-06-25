3a52cef829c0858e605a34ccdc9767179f3c6f7e
Segura Mendoza Gael Alejandro <gaelsegura.mendoza@gmail.com>
2024-06-24 18:15:21 -0600

    Add menu in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
 import "./Navbar.css"
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleClick(event) {
+    const target = event.target
+    if(target.tagName !== "A") return
+
+    setIsOpen(false)
+  }
+
   return (
     <header>
       <nav className="navbar">
@@ -11,6 +21,19 @@ export default function Header() {
           <li><a href="#presentation"> Contacto </a></li>
         </ul>
       </nav>
+
+      <nav className="navbar show">
+        <div className="hamburger">
+          <i className="fa-solid fa-bars-staggered" onClick={() => setIsOpen(true)}/>
+        </div>
+      </nav>
+
+      <ul className={`mobile ${isOpen ? "open" : ""}`} onClick={handleClick} >
+        <li><a href="#about-me"> Perfil </a></li>
+        <li><a href="#experience"> Experiencia </a></li>
+        <li><a href="#projects"> Proyectos </a></li>
+        <li><a href="#presentation"> Contacto </a></li>
+      </ul>
     </header>
   )
 }
